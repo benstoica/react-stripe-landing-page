9e4bb6a7fcf007a89d6afd04f61b43fb89323304
Ben Stoica <bstoica@uwm.com>
2022-01-12 08:24:07 -0500

    setting up context boilerplate

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import Sidebar from "./Sidebar";
 import Submenu from "./Submenu";
 
 function App() {
-  return <h1>test</h1>;
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
+    </>
+  );
 }
 
 export default App;
